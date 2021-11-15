a632f90f9c1b7f2717bfcd552e1159607bbe9984
Tzunami <AmiRajpal@protonmail.com>
2021-11-14 20:59:33 -0500

    Debugger: Added Agent, Agent2, Client2, & Creator2

--- init.sh
+++ init.sh
@@ -3,8 +3,9 @@
 #NOTE: . ./init.sh
 # Don't forget the first '.' to carry over to transactions.sh
 
-echo "========== Basic Setup: Flow Accounts, Contracts, & Funds"
+echo "========== Basic Setup: Flow Accounts, Contracts, & Funds =========="
 
+echo "---------- Setup: Public Keys ----------"
 export CREATOR_PUBKEY=0x$(tail -1 ./keys/creator_keys   | awk '{print $3}' | tr -d '\n')
 export ADMIN_PUBKEY=$(tail -1 ./keys/admin_keys         | awk '{print $3}' | tr -d '\n')
 export CLIENT_PUBKEY=$(tail -1 ./keys/client_keys       | awk '{print $3}' | tr -d '\n')
@@ -17,6 +18,12 @@ export DAAM_NFT_PUBKEY=$(tail -1 ./keys/daam_nft_keys   | awk '{print $3}' | tr 
 export AGENCY_PUBKEY=$(tail -1 ./keys/agency_keys       | awk '{print $3}' | tr -d '\n')
 export CTO_PUBKEY=$(tail -1 ./keys/cto_keys             | awk '{print $3}' | tr -d '\n')
 
+export AGENT_PUBKEY=$(tail -1 ./keys/agent_keys         | awk '{print $3}' | tr -d '\n')
+export AGENT2_PUBKEY=$(tail -1 ./keys/agent2_keys       | awk '{print $3}' | tr -d '\n')
+export CREATOR2_PUBKEY=$(tail -1 ./keys/creator2_keys   | awk '{print $3}' | tr -d '\n')
+export CLIENT2_PUBKEY=$(tail -1 ./keys/client2_keys     | awk '{print $3}' | tr -d '\n')
+
+echo "---------- Setup: Priavte Keys ----------"
 export CREATOR_PRIVKEY=$(tail -2 ./keys/creator_keys     | awk '{print $3}' | tr -d '\n')
 export ADMIN_PRIVKEY=$(tail -2 ./keys/admin_keys         | awk '{print $3}' | tr -d '\n')
 export CLIENT_PRIVKEY=$(tail -2 ./keys/client_keys       | awk '{print $3}' | tr -d '\n')
@@ -28,6 +35,11 @@ export ADMIN2_PRIVKEY=$(tail -2 ./keys/admin2_keys       | awk '{print $3}' | tr
 export DAAM_NFT_PRIVKEY=$(tail -2 ./keys/daam_nft_keys   | awk '{print $3}' | tr -d '\n')
 export AGENCY_PRIVKEY=$(tail -2 ./keys/agency_keys       | awk '{print $3}' | tr -d '\n')
 export CTO_PRIVKEY=$(tail -2 ./keys/cto_keys             | awk '{print $3}' | tr -d '\n')
+
+export AGENT_PRIVKEY=$(tail -2 ./keys/agent_keys         | awk '{print $3}' | tr -d '\n')
+export AGENT2_PRIVKEY=$(tail -2 ./keys/agent2_keys       | awk '{print $3}' | tr -d '\n')
+export CREATOR2_PRIVKEY=$(tail -2 ./keys/creator2_keys   | awk '{print $3}' | tr -d '\n')
+export CLIENT2_PRIVKEY=$(tail -2 ./keys/client2_keys     | awk '{print $3}' | tr -d '\n')
 
 # init accounts; Must be in order
 flow accounts create --key $ADMIN_PUBKEY --save admin
@@ -42,19 +54,32 @@ flow accounts create --key $DAAM_NFT_PUBKEY --save daam_nft
 flow accounts create --key $AGENCY_PUBKEY --save agency
 flow accounts create --key $CTO_PUBKEY --save cto
 
+flow accounts create --key $AGENT_PUBKEY --save agent
+flow accounts create --key $AGENT2_PUBKEY --save agent2
+flow accounts create --key $CREATOR2_PUBKEY --save creator2
+flow accounts create --key $CLIENT2_PUBKEY --save client2
+
 # Get & print Address
 export CREATOR=$(head -1 creator | awk '{print $2}')
 echo Creator: $CREATOR
+export CREATOR2=$(head -1 creator2 | awk '{print $2}')
+echo Creator2: $CREATOR2
 export ADMIN=$(head -1 admin   | awk '{print $2}')
 echo Admin:   $ADMIN
+export ADMIN2=$(head -1 admin2 | awk '{print $2}')
+echo Admin2: $ADMIN2
+export AGENT=$(head -1 agent   | awk '{print $2}')
+echo Agent:   $AGENT
+export AGENT2=$(head -1 agent2 | awk '{print $2}')
+echo Agent2: $AGENT2
 export NOBODY=$(head -1 nobody | awk '{print $2}')
 echo Nobody: $NOBODY
 export CLIENT=$(head -1 client | awk '{print $2}')
 echo Client: $CLIENT
+export CLIENT2=$(head -1 client2 | awk '{print $2}')
+echo Client2: $CLIENT2
 export MARKETPLACE=$(head -1 marketplace     | awk '{print $2}')
 echo Marketplace: $MARKETPLACE
-export ADMIN2=$(head -1 admin2 | awk '{print $2}')
-echo Copyright: $ADMIN2
 export NFT=$(head -1 nft        | awk '{print $2}')
 echo NFT: $NFT
 export PROFILE=$(head -1 profile | awk '{print $2}')
@@ -66,26 +91,33 @@ echo Agency: $AGENCY
 export CTO=$(head -1 cto | awk '{print $2}')
 echo CTO: $CTO
 
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $CREATOR
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $ADMIN
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $NOBODY
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $NFT
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $DAAM_NFT
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $CREATOR
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $ADMIN
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $NOBODY
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $NFT
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $DAAM_NFT
 
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $MARKETPLACE
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $CLIENT
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $ADMIN2
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $PROFILE
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $MARKETPLACE
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $CLIENT
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $ADMIN2
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $PROFILE
 
-flow transactions send ./transactions/send_flow_em.cdc 199.999 $CTO
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $CTO
+
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $CREATOR2
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $CLIENT2
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $AGENT
+flow transactions send ./transactions/send_flow_em.cdc 200.0 $AGENT2
 
 # Init Contracts
 
 # FUSD Enulator Contract
+echo "========== Publish Test FUSD Contract =========="
 flow accounts add-contract FUSD ./contracts/FUSD.cdc --signer profile
 flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer cto
-flow transactions send ./transactions/setup_fusd.cdc 1000000.0 $CTO --signer profile
+flow transactions send ./transactions/setup_fusd.cdc 100000000.0 $CTO --signer profile
 
+echo "========== Send 100K FUSD to All Accounts =========="
 flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer agency
 
 flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer creator
@@ -98,6 +130,11 @@ flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer client
 flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer admin2
 flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer profile
 
+flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer agent
+flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer agent2
+flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer creator2
+flow transactions send ./transactions/fusd/setup_fusd_vault.cdc --signer client2
+
 flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $CREATOR --signer cto
 flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $ADMIN --signer cto
 flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $NOBODY --signer cto
@@ -108,7 +145,13 @@ flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $CLIENT --
 flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $ADMIN2 --signer cto
 flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $PROFILE --signer cto
 
+flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $CREATOR2 --signer cto
+flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $CLIENT2 --signer cto
+flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $AGENT --signer cto
+flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $AGENT2 --signer cto
+
 # Emulator Contracts
+echo "========== Publish Supporting Contracts: NonFungibleToken & Profile"
 flow accounts add-contract NonFungibleToken ./contracts/NonFungibleToken.cdc
 flow accounts add-contract Profile ./contracts/Profile.cdc --signer profile
 
@@ -122,8 +165,6 @@ flow accounts update-contract DAAM ./contracts/daam_nft.cdc --signer daam_nft
 #Auction
 flow accounts add-contract AuctionHouse ./contracts/auction.cdc --signer marketplace
 
-echo "----------- All Contracts Are Published and Flow Accounts Setup with FUSD Funds. -----------"
-
 echo "========== SETUP ALL TYPES OF ACCOUNTS: DAAM, Profile, AuctionWallet  ==========" 
 
 # Setup Profiles
@@ -134,7 +175,11 @@ flow transactions send ./transactions/create_profile.cdc --signer admin2
 flow transactions send ./transactions/create_profile.cdc --signer creator
 flow transactions send ./transactions/create_profile.cdc --signer client
 flow transactions send ./transactions/create_profile.cdc --signer nobody
-# CTO has already been done previously. see invite Admin/CTO
+
+flow transactions send ./transactions/create_profile.cdc --signer agent
+flow transactions send ./transactions/create_profile.cdc --signer agent2
+flow transactions send ./transactions/create_profile.cdc --signer creator2
+flow transactions send ./transactions/create_profile.cdc --signer client2
 
 # Setup DAAM Accounts
 echo "========= Setup All DAAM Accounts ========="
@@ -145,6 +190,11 @@ flow transactions send ./transactions/setup_daam_account.cdc --signer admin
 flow transactions send ./transactions/setup_daam_account.cdc --signer admin2
 flow transactions send ./transactions/setup_daam_account.cdc --signer cto
 
+flow transactions send ./transactions/setup_daam_account.cdc --signer agent
+flow transactions send ./transactions/setup_daam_account.cdc --signer agent2
+flow transactions send ./transactions/setup_daam_account.cdc --signer client
+flow transactions send ./transactions/setup_daam_account.cdc --signer creator2
+
 # Setup Auction Wallets
 echo "========= Setup All Auction Wallets ========="
 flow transactions send ./transactions/auction/create_auction_wallet.cdc --signer nobody
@@ -153,12 +203,19 @@ flow transactions send ./transactions/auction/create_auction_wallet.cdc --signer
 flow transactions send ./transactions/auction/create_auction_wallet.cdc --signer admin2
 flow transactions send ./transactions/auction/create_auction_wallet.cdc --signer cto
 
+flow transactions send ./transactions/auction/create_auction_wallet.cdc --signer agent
+flow transactions send ./transactions/auction/create_auction_wallet.cdc --signer agent2
+flow transactions send ./transactions/auction/create_auction_wallet.cdc --signer creator2
+flow transactions send ./transactions/auction/create_auction_wallet.cdc --signer client2
+
 # Answer Default Admin / CTO
 echo "Answer default Admin Invite created by contract creation."
 flow transactions send ./transactions/answer_admin_invite.cdc true --signer cto
 
 # Setup AuctionHouse Minter Key
 echo "Send AuctionHouse Minter Key."
-flow transactions send ./transactions/admin/invite_minter.cdc $MARKETPLACE --signer admin
+flow transactions send ./transactions/admin/invite_minter.cdc $MARKETPLACE --signer cto
 echo "AuctionHouse Accepts Minter Key."
 flow transactions send ./transactions/answer_minter_invite.cdc true --signer marketplace
+
+echo "----------- All Contracts Are Published with Flow Accounts FUSD Funded. -----------"
