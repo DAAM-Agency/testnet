c60781518b05d55dec700623e80bcde099312d04
Tzunami <AmiRajpal@Protonmail.com>
2022-02-21 17:52:57 -0500

    fixed type-os

--- debug/extended_auctions.sh
+++ debug/extended_auctions.sh
@@ -63,7 +63,7 @@ flow transactions send ./transactions/auction/create_original_auction.cdc 7 $STA
 
 # Verify Metadata
 echo "========= Veriy Metadata ========="
-flow scripts execute ./scripts/wallet/get_tokenIds.cdc $CREATOR
+flow scripts execute ./scripts/wallet/get_tokenIDs.cdc $CREATOR
 
 # Reset Copyright
 echo "========= Reset Copyright ========="
