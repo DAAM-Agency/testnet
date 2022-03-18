df6d08ac05872d5f242c9b13217d133121ba4969
Tzunami <AmiRajpal@protonmail.com>
2022-03-17 23:12:55 -0400

    updated: view_basic_data.sh; use jq json now

--- debug/view_basic_data.sh
+++ debug/view_basic_data.sh
@@ -6,47 +6,47 @@ getAddressName() {
     case $1 in
 
         $AGENCY)
-        echo -n "Agency "
+        echo -n " Agency  : "
         ;;
 
         $ADMIN)
-        echo -n "Admin "
+        echo -n " Admin   : "
         ;;
         
         $ADMIN2)
-        echo -n "Admin2 "
+        echo -n " Admin2  : "
         ;;
         
         $AGENT)
-        echo -n "Agent "
+        echo -n " Agent   : "
         ;;
         
         $AGENT2)
-        echo -n "Agent2 "
+        echo -n " Agent2  : "
         ;;
         
         $CREATOR)
-        echo -n "Creator "
+        echo -n " Creator : "
         ;;
         
         $CREATOR2)
-        echo -n "Creator2 "
+        echo -n " Creator2: "
         ;;
         
         $NOBODY)
-        echo -n "Nobody "
+        echo -n " Nobody  : "
         ;;
         
         $CTO)
-        echo -n "CTO "
+        echo -n " CTO     : "
         ;;
         
         $CLIENT)
-        echo -n "Client "
+        echo -n " Client  : "
         ;;
         
         $CLIENT2)
-        echo -n "Client2 "
+        echo -n " Client2 : "
         ;;
         
         *)
@@ -61,33 +61,33 @@ echo "---------- FUSD ----------"
 for user in $AGENCY $CREATOR $CREATOR2 $AGENT $AGENT2 $CLIENT $CLIENT2
 do
     getAddressName $user
-    flow scripts execute ./scripts/get_fusd_balance.cdc $user
+    flow -o json scripts execute ./scripts/get_fusd_balance.cdc $user | jq ' .value | .value'
 done
 
 # Get all Auctions
-echo "---------- Auction Wallet ----------"
+echo "---------- Auction Wallet ----------\n"
 for user in $CREATOR $CREATOR2 $CLIENT $CLIENT2 $NOBODY
 do
     getAddressName $user
-    flow scripts execute ./scripts/auction/get_auctions.cdc $user
+    flow -o json scripts execute ./scripts/auction/get_auctions.cdc $user | jq ' .value'
 done
 
 # Verify Collection
-echo "---------- Verify Collections ----------"
+echo "---------- Verify Collections ----------\n"
 for user in $CREATOR $CREATOR2 $CLIENT $CLIENT2 $NOBODY
 do
     getAddressName $user
-    flow scripts execute ./scripts/daam_wallet/get_collections.cdc $user 
+    flow -o json scripts execute ./scripts/daam_wallet/get_collections.cdc $user | jq ' .value | .value'
 done
 
 # Verify Metadata
-echo "---------- Veriy Metadata ----------"
+echo "---------- Veriy Metadata ----------\n"
 ./scripts/get_mids.sh | jq -c
 
 # Verify TokenIDs
-echo "---------- Veriy TokenIDs ----------"
+echo "---------- Veriy TokenIDs ----------\n"
 for user in $CREATOR $CREATOR2 $AGENT $AGENT2 $CLIENT $CLIENT2 $NOBODY
 do
     getAddressName $user
-    flow scripts execute ./scripts/daam_wallet/get_tokenIDs.cdc $user
+    flow -o json scripts execute ./scripts/daam_wallet/get_tokenIDs.cdc $user | jq ' .value | .value'
 done
