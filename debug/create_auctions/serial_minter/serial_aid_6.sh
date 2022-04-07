ea334b8ab828269359b39849a9d692abcfaaf802
Tzunami <AmiRajpal@protonmail.com>
2022-04-07 00:17:12 -0400

    updated: serial_aid_6, added $aid

--- debug/create_auctions/serial_minter/serial_aid_6.sh
+++ debug/create_auctions/serial_minter/serial_aid_6.sh
@@ -1,11 +1,13 @@
+aid=6
+
 # Test Serial Minter
-echo "========== Testing Serial Minter AID: 6 =========="
+echo "========== Testing Serial Minter AID: "$aid" =========="
 flow transactions send ./transactions/send_flow_em.cdc 1.0 $PROFILE  # dummy action update bc
 
-echo "---------- buyItNow (1): Nobody AID: 6 ----------"
-BUYITNOW=$(flow scripts execute ./scripts/auction/get_buy_now_amount.cdc $CREATOR2 6 $NOBODY | awk '{print $2}')
+echo "---------- BuyItNow Count: 1, Bidder: Nobody, AID: "$aid" ----------"
+BUYITNOW=$(flow scripts execute ./scripts/auction/get_buy_now_amount.cdc $CREATOR2 $aid $NOBODY | awk '{print $2}')
 echo BUYITNOW: $BUYITNOW
-flow transactions send ./transactions/auction/buy_it_now.cdc $CREATOR2 6 $BUYITNOW --signer nobody #H
+flow transactions send ./transactions/auction/buy_it_now.cdc $CREATOR2 $aid $BUYITNOW --signer nobody #H
 
 echo "NOBODY FUSD"
 flow transactions send ./transactions/send_flow_em.cdc 1.0 $PROFILE  # dummy action update bc
@@ -15,17 +17,16 @@ echo "--------- Get Creator Auctions ---------"
 flow scripts execute ./scripts/auction/get_auctions.cdc $CREATOR2
 
 echo "========== Testing: endReprint =========="
-flow transactions send ./transactions/auction/end_reprints.cdc 6 --signer creator2
+flow transactions send ./transactions/auction/end_reprints.cdc $aid --signer creator2
 flow transactions send ./transactions/send_flow_em.cdc 1.0 $PROFILE  # dummy action update bc
 
-echo "---------- BuyItNow (2) 30.6, Nobody AID: 6 ----------"
-
-BUYITNOW=$(flow scripts execute ./scripts/auction/get_buy_now_amount.cdc $CREATOR2 6 $NOBODY | awk '{print $2}')
+echo "---------- BuyItNow Count: 2, Bidder: Nobody, AID: "$aid" ----------"
+BUYITNOW=$(flow scripts execute ./scripts/auction/get_buy_now_amount.cdc $CREATOR2 $aid $NOBODY | awk '{print $2}')
 echo BUYITNOW: $BUYITNOW
-flow transactions send ./transactions/auction/buy_it_now.cdc $CREATOR2 6 $BUYITNOW --signer nobody #H
+flow transactions send ./transactions/auction/buy_it_now.cdc $CREATOR2 $aid $BUYITNOW --signer nobody #H
 
-echo "FAIL TEST: No more reprints due to endReprint (previous) AID: 6"
-flow transactions send ./transactions/auction/buy_it_now.cdc $CREATOR2 6 30.6 --signer nobody #H
+echo "FAIL TEST: No more reprints due to endReprint (previous) AID: "$aid
+flow transactions send ./transactions/auction/buy_it_now.cdc $CREATOR2 $aid 30.$aid --signer nobody #H
 flow transactions send ./transactions/send_flow_em.cdc 1.0 $PROFILE  # dummy action update bc
 
 echo "--------- Get Creator Auctions ---------"
