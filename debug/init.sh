a2c688c2898865906a22eebd78cf0831bef0d89e
Tzunami <AmiRajpal@protonmail.com>
2022-05-25 18:50:20 -0400

    added Metadataview to bash debugger

--- debug/init.sh
+++ debug/init.sh
@@ -166,6 +166,7 @@ flow transactions send ./transactions/fusd/transfer_fusd.cdc 100000.0 $AGENT2 --
 echo "========== Publish Supporting Contracts: NonFungibleToken & Profile"
 flow accounts add-contract NonFungibleToken ./contracts/NonFungibleToken.cdc
 flow accounts add-contract Profile ./contracts/Profile.cdc --signer profile
+flow accounts add-contract MetadataViews ./contracts/MetadataViews.cdc
 
 echo "========= Publish DAAM Contracts =========="
 # Categories
