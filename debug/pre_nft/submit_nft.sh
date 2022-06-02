a3f05a4f880a7f9072fb9fad628b1091bad52bfb
Tzunami <AmiRajpal@Protonmail.com>
2022-06-02 19:39:26 -0400

    updated: get_tokenID.cdc

--- debug/pre_nft/submit_nft.sh
+++ debug/pre_nft/submit_nft.sh
@@ -5,7 +5,7 @@ echo "========= Submit NFTs ========="
 flow transactions send ./transactions/creator/submit_accept.cdc "Name A" 2 '["Digital","Image"]' nil "description A" false  "thumbnail A" "file" false  "file A" "file" 0.10 --signer creator # Will be MID 1
 flow transactions send ./transactions/creator/submit_accept.cdc "Name B" 7 '["Digital","Image"]' nil "description B" false  "thumbnail B" "file" false  "file B" "file" 0.12 --signer creator # Will be MID 2
 flow transactions send ./transactions/creator/submit_accept.cdc "Name C" 2 '["Digital","Image"]' nil "description C" false  "thumbnail C" "file" false  "file C" "file" 0.13 --signer creator # Will be MID 3
-flow transactions send ./transactions/creator/submit_accept.cdc "Name D" 0 '["Digital","Image"]' nil "description D" false  "thumbnail D" "file" false  "file D" "file" 0.14 --signer creator # Will be MID 4
+flow transactions send ./transactions/creator/submit_accept.cdc "Name D" nil '["Digital","Image"]' nil "description D" false  "thumbnail D" "file" false  "file D" "file" 0.14 --signer creator # Will be MID 4
 flow transactions send ./transactions/creator/submit_accept.cdc "Name E" 3 '["Digital","Image"]' nil "description E" false  "thumbnail E" "file" false  "file E" "file" 0.15 --signer creator # Will be MID 5
 flow transactions send ./transactions/creator/submit_accept.cdc "Name F" 2 '["Digital","Image"]' nil "description F" false  "thumbnail F" "file" false  "file F" "file" 0.16 --signer creator2 # Will be MID 6
 flow transactions send ./transactions/creator/submit_accept.cdc "Name G" 2 '["Digital","Image"]' nil "description G" false  "thumbnail G" "file" false  "file G" "file" 0.17 --signer creator2 # Will be MID 7
--- scripts/daam_wallet/get_tokenIDs.cdc
+++ scripts/daam_wallet/get_tokenIDs.cdc
@@ -1,10 +1,11 @@
 // get_tokenIDs.cdc
 
+import NonFungibleToken from 0xf8d6e0586b0a20c7
 import DAAM from 0xfd43f9148d4b725d
 
 pub fun main(account: Address): [UInt64]? {
     let collectionRef = getAccount(account)
-        .getCapability<&DAAM.Collection{DAAM.CollectionPublic}>(DAAM.collectionPublicPath)
+        .getCapability<&DAAM.Collection{NonFungibleToken.CollectionPublic}>(DAAM.collectionPublicPath)
         .borrow()
         //?? panic("Could not borrow capability from public collection")
     
