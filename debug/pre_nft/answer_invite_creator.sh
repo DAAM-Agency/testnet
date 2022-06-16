6186b0fe0c77b19a0c199ebfa32e3dbfdfea16c9
Tzunami <AmiRajpal@Protonmail.com>
2022-06-16 00:53:07 -0400

    re-init

--- debug/pre_nft/answer_invite_creator.sh
+++
@@ -1,9 +0,0 @@
-#Creator
-echo -n "Verify Creator Status: "
-flow scripts execute ./scripts/is_creator.cdc $2
-
-echo "---------- Answering Creators ----------"
-flow transactions send ./transactions/answer/answer_creator_invite.cdc $3 --signer $1
-
-echo -n "Verify Creator Status: "
-flow scripts execute ./scripts/is_creator.cdc $2
