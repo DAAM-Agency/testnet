faf8cf58820ed189c358620caecd542683fb4524
tzunami <AmiRajpal@protonmal.com>
2022-07-13 22:43:55 -0400

    updated management.sh

--- management.sh
+++ management.sh
@@ -1,21 +1,23 @@
 
 #!/bin/bash
 
-flow transactions send ./transactions/auction/close_all_auctions.cdc --signer cto
+flow transactions send ./transactions/auction/close_all_auctions.cdc --signer $1 --gas-limit 9999 #emulator
 LAST_HEIGHT=$(flow -o json blocks get latest | jq -r ' .height')
 CURRENT_HEIGHT=$(flow -o json blocks get latest | jq -r ' .height')
+echo "AuctionHouse Management Running..."
+echo "Signer:" $1
 echo "Press Ctrl-C to Quit"
 
 while true; do
+    sleep 1
 	CURRENT_HEIGHT=$(flow -o json blocks get latest | jq -r ' .height')
+	echo $LAST_HEIGHT $CURRENT_HEIGHT
 	if [ `expr $LAST_HEIGHT \< $CURRENT_HEIGHT` == 1 ]; then
-		echo "in if"
-		#flow -o json events get A.01837e15023c9249.AuctionHouse.AuctionCreated --last $(expr $CURRENT_HEIGHT - $LAST_HEIGHT) # testnet
-		AUCTIONS=$(flow events get A.045a1763c93006ca.AuctionHouse.AuctionCreated --last $(expr $CURRENT_HEIGHT - $LAST_HEIGHT))  # emulator
-		echo Auctions=$AUCTIONS
-		if [ -z $AUCTIONS  ]; then
-			echo "GOAL"
-			#flow transactions send transactions send ./transactions/auction/close_all_auctions.cdc --signer cto
+		#AUCTIONS=$(flow -o json -n testnet events get A.01837e15023c9249.AuctionHouse.ItemWon --last $(expr $CURRENT_HEIGHT - $LAST_HEIGHT))  # testnet
+		AUCTIONS=$(flow -o json events get A.045a1763c93006ca.AuctionHouse.ItemWon --last $(expr $CURRENT_HEIGHT - $LAST_HEIGHT))  # emulator
+		echo inif Auctions=$AUCTIONS
+		if [ $AUCTIONS != []  ]; then
+			flow transactions send ./transactions/auction/close_all_auctions.cdc --signer $1 --gas-limit 9999 #emulator
 		fi
 		LAST_HEIGHT=$(flow -o json blocks get latest | jq -r ' .height')
 	fi
