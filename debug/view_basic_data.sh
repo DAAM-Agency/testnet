f6593f0c25627985cde625ca27c97f4cbffe9895
tzunami <AmiRajpal@protonmail.com>
2022-08-02 16:58:26 -0400

    update: get_history & get_sale_history

--- debug/view_basic_data.sh
+++ debug/view_basic_data.sh
@@ -106,9 +106,10 @@ echo "========= Verify Metadata ========="
 for user in $CREATOR $CREATOR2
 do
     METADATA=$(flow -o json scripts execute ./scripts/get_mids.cdc $user | jq ' .value' | grep value | awk '{print $2}' | tr -d '"')
-    #echo "Metadata: "$METADATA
+    echo "Metadata: "$METADATA
     for list in $METADATA
     do
+        echo $list $user
         flow scripts execute ./scripts/view_metadata.cdc $user $list
     done
 done
