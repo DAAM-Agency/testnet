fb9737dba941ec8e735c7741b91b34012a195ac7
tzunami <AmiRajpal@Protonmail.com>
2022-08-25 02:02:10 -0400

    Pre-DAAM_V22

--- debug/pre_nft/invite_admin.sh
+++
@@ -1,8 +0,0 @@
-# Invite Admin & Accept
-echo "========= Invite Admins ========="
-for user in $@
-do
-    echo "Inviting: "$user
-    flow transactions send ./transactions/admin/invite_admin.cdc $user --signer cto
-done
-flow transactions send ./transactions/send_flow_em.cdc 1.0 $PROFILE  # dummy action update bc
--- debug/pre_nft/invite_creator.sh
+++
@@ -1,7 +0,0 @@
-# Invite Admin & Accept
-echo "========= Invite Creator ========="
-for user in $CREATOR $CREATOR2
-do
-    echo "Inviting: "$user
-    flow transactions send ./transactions/admin/invite_creator.cdc $user 0.21 --signer agent
-done
