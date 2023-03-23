1acde8bbac845af8ba7d77582fc81b4255ccc1a3
tzunami <AmiRajpal@protonmail.com>
2023-03-23 14:09:10 -0400

    updated

--- debug/pre_nft/invite_creator.sh
+++ debug/pre_nft/invite_creator.sh
@@ -1,5 +1,5 @@
 # Invite Admin & Accept
 echo "========= Invite Creator ========="
-flow transactions send ./transactions/admin/invite_creator_by_agent.cdc $CREATOR  0.21 --signer agent
-flow transactions send ./transactions/admin/invite_creator_by_agent.cdc $CREATOR2 0.11 --signer agent2
+flow transactions send ./transactions/admin/invite_creator.cdc $CREATOR  0.21 --signer agent
+flow transactions send ./transactions/admin/invite_creator.cdc $CREATOR2 0.11 --signer agent2
  
